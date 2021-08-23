4106d780de2884f4b62f5e85d2837080a9587bb0
Jesse De Jong <jdoceau@gmail.com>
2021-08-23 17:40:57 +1000

    Wrote category post route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -30,8 +30,14 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
   // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const newCategory = await Category.create(req.body)
+    res.status(200).json(newCategory);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
 router.put('/:id', (req, res) => {
